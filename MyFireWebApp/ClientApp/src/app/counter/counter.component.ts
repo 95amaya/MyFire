8e2b09fe6fb900bee0dd0f7e2deae933ab684931
Michael Amaya <michael.amaya09@gmail.com>
2023-04-16 03:13:39 -0500

    Added Tailwind and NgSelect

--- MyFireWebApp/ClientApp/src/app/counter/counter.component.ts
+++ MyFireWebApp/ClientApp/src/app/counter/counter.component.ts
@@ -3,21 +3,50 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-counter-component',
   template: `
-    <h1>Counter</h1>
-
-    <p>This is a simple example of an Angular component.</p>
-
-    <p aria-live="polite">
-      Current count: <strong>{{ currentCount }}</strong>
-    </p>
-
-    <button class="btn btn-primary" (click)="incrementCounter()">
-      Increment
-    </button>
+    <div class="h4">Component Libray Examples</div>
+    <div class="grid grid-cols-4">
+      <div class="p-3 flex flex-column justify-around">
+        <div class="h5">Button</div>
+        <button class="btn btn-primary" (click)="incrementCounter()">
+          Increment
+        </button>
+        {{ currentCount }}
+      </div>
+      <div class="p-3 flex flex-column justify-around">
+        <div class="h5">Date</div>
+        <input type="date" [(ngModel)]="myDate" />
+        <div [ngClass]="{ show: !!myDate }">
+          {{ myDate }}
+        </div>
+      </div>
+      <div class="p-3 flex flex-column justify-around">
+        <div class="h5">Dropdown</div>
+        <ng-select
+          [items]="carOptions"
+          bindLabel="name"
+          bindValue="id"
+          [(ngModel)]="selectedCar"
+        >
+        </ng-select>
+        <div *ngIf="selectedCar; else emptySelectedOption">
+          {{ selectedCar }}
+        </div>
+        <ng-template #emptySelectedOption> No Option Selected </ng-template>
+      </div>
+    </div>
   `,
 })
 export class CounterComponent {
   public currentCount = 0;
+  public myDate = null;
+  public selectedCar = null;
+
+  public carOptions = [
+    { id: 1, name: 'Volvo' },
+    { id: 2, name: 'Saab' },
+    { id: 3, name: 'Opel' },
+    { id: 4, name: 'Audi' },
+  ];
 
   public incrementCounter() {
     this.currentCount++;
--- MyFireWebApp/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ MyFireWebApp/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -2,53 +2,64 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav-menu',
-  styleUrls: ['./nav-menu.component.css'],
-  template: `
-    <header>
-      <nav
-        class="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3"
+  template: ` <nav
+    class="bg-white border-gray-200 dark:bg-gray-900 shadow-sm mb-4"
+  >
+    <div
+      class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto px-4 py-2"
+    >
+      <a class="flex items-center h3" [routerLink]="['/']">MyFireWebApp</a>
+      <button
+        data-collapse-toggle="navbar-default"
+        type="button"
+        class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        aria-controls="navbar-default"
+        aria-expanded="false"
+        (click)="toggle()"
       >
-        <div class="container">
-          <a class="navbar-brand" [routerLink]="['/']">MyFireWebApp</a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target=".navbar-collapse"
-            aria-label="Toggle navigation"
-            [attr.aria-expanded]="isExpanded"
-            (click)="toggle()"
+        <svg
+          class="w-6 h-6"
+          aria-hidden="true"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </button>
+      <div
+        class="w-full md:block md:w-auto"
+        id="navbar-default"
+        [ngClass]="{ show: isExpanded, hidden: !isExpanded }"
+      >
+        <ul
+          class="font-medium flex flex-col p-2 md:p-0 md:flex-row md:space-x-8 md:mt-0 md:border-0"
+        >
+          <li
+            class="nav-item"
+            [routerLinkActive]="['link-active']"
+            [routerLinkActiveOptions]="{ exact: true }"
           >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div
-            class="navbar-collapse collapse d-sm-inline-flex justify-content-end"
-            [ngClass]="{ show: isExpanded }"
-          >
-            <ul class="navbar-nav flex-grow">
-              <li
-                class="nav-item"
-                [routerLinkActive]="['link-active']"
-                [routerLinkActiveOptions]="{ exact: true }"
-              >
-                <a class="nav-link text-dark" [routerLink]="['/']">Home</a>
-              </li>
-              <li class="nav-item" [routerLinkActive]="['link-active']">
-                <a class="nav-link text-dark" [routerLink]="['/counter']"
-                  >Counter</a
-                >
-              </li>
-              <li class="nav-item" [routerLinkActive]="['link-active']">
-                <a class="nav-link text-dark" [routerLink]="['/fetch-data']"
-                  >Fetch data</a
-                >
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </header>
-  `,
+            <a class="nav-link text-dark" [routerLink]="['/']">Home</a>
+          </li>
+          <li class="nav-item" [routerLinkActive]="['link-active']">
+            <a class="nav-link text-dark" [routerLink]="['/counter']"
+              >Counter</a
+            >
+          </li>
+          <li class="nav-item" [routerLinkActive]="['link-active']">
+            <a class="nav-link text-dark" [routerLink]="['/fetch-data']"
+              >Fetch data</a
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>`,
 })
 export class NavMenuComponent {
   isExpanded = false;
@@ -58,6 +69,7 @@ export class NavMenuComponent {
   }
 
   toggle() {
+    console.log(this.isExpanded);
     this.isExpanded = !this.isExpanded;
   }
 }
